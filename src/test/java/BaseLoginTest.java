bef3590b4dc695a2fe8364062ed96066dcdd9fcd
denistaranchik <taranchikdenis94@gmail.com>
2026-08-12 10:46:30 +0300

    add screenshot listener (screenshots on failure)

--- src/test/java/BaseLoginTest.java
+++ src/test/java/BaseLoginTest.java
@@ -3,9 +3,12 @@ import config.ConfigReader;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
+import listeners.ScreenshotListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(ScreenshotListener.class)
 public class BaseLoginTest {
 
     public Playwright playwright;
---
+++ src/test/java/listeners/ScreenshotListener.java
@@ -0,0 +1,36 @@
+package listeners;
+
+import Page.BasePage;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.ScreenshotType;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ScreenshotListener implements ITestListener {
+
+    private static final String SCREENSHOT_DIR = "screenshots";
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Page page = BasePage.page;
+        if (page == null) {
+            return;
+        }
+
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        String fileName = result.getName() + "_" + timestamp + ".png";
+        Path path = Paths.get(SCREENSHOT_DIR, fileName);
+
+        page.screenshot(new Page.ScreenshotOptions()
+                .setPath(path)
+                .setType(ScreenshotType.PNG)
+                .setFullPage(true));
+
+        System.out.println("Failure screenshot saved: " + path.toAbsolutePath());
+    }
+}
