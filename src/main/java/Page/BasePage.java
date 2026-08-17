dd827bb0cdaa5e075c182b44c44b5c9039ea76c1
denistaranchik <taranchikdenis94@gmail.com>
2026-08-17 23:20:04 +0300

    Add static fillInput(Locator, String) method for using it in fill methods across the project and add locators which can be used in different pages

--- src/main/java/Page/BasePage.java
+++ src/main/java/Page/BasePage.java
@@ -1,9 +1,17 @@
 package Page;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-
 
 public class BasePage {
 
+    public final Locator emailInput = BasePage.page.locator("#Email");
+    public final Locator passwordInput = BasePage.page.locator("#Password");
+
+    public static void fillInput(Locator locator, String string) {
+        locator.fill(string);
+    }
+
     public static Page page;
+
 }
