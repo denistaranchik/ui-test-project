a9571b7881df1f41f84d4b2f0eb477ed54c816b1
denistaranchik <taranchikdenis94@gmail.com>
2026-08-18 22:20:07 +0300

    refactor RegistrationPage by updating clickGenderRadioButton for using it in registration page with DataProvider / delete Email/Password locator for using it from BasePage / update fill methods for using fillInput method from BasePage

--- src/main/java/Page/RegistrationPage.java
+++ src/main/java/Page/RegistrationPage.java
@@ -1,64 +1,53 @@
 package Page;
 
 import com.microsoft.playwright.Locator;
-import java.util.Random;
-import org.apache.commons.lang3.RandomStringUtils;
 
-
-public class RegistrationPage {
+public class RegistrationPage extends BasePage {
 
     private final Locator genderMaleRadioButton = BasePage.page.locator("#gender-male");
     private final Locator genderFemaleRadioButton = BasePage.page.locator("#gender-female");
     private final Locator firstNameInput = BasePage.page.locator("#FirstName");
     private final Locator lastNameInput = BasePage.page.locator("#LastName");
-    private final Locator emailInput = BasePage.page.locator("#Email");
-
-    private final Locator passwordInput = BasePage.page.locator("#Password");
     private final Locator confirmPasswordInput = BasePage.page.locator("#ConfirmPassword");
-    private final Locator registrationButton = BasePage.page.locator("#register-button");
+    private final Locator confirmRegistrationButton = BasePage.page.locator("#register-button");
 
-
-    // Click methods
-
-    public RegistrationPage clickGenderMaleRadioButton() {
-        genderMaleRadioButton.click();
-        return this;
+    public RegistrationPage clickGenderRadioButton(String gender) {
+        if (gender.equalsIgnoreCase("male")) {
+            genderMaleRadioButton.click();
+            return this;
+        } else {
+            genderFemaleRadioButton.click();
+            return this;
+        }
     }
 
-    public RegistrationPage clickGenderFemaleRadioButton() {
-        genderFemaleRadioButton.click();
-        return this;
-    }
-
-    public HomePage clickRegistrationButton() {
-        registrationButton.click();
+    public HomePage clickConfirmRegistrationButton() {
+        confirmRegistrationButton.click();
         return new HomePage();
     }
 
-    // Fill methods
-
     public RegistrationPage fillFirstNameInput(String firstName) {
-        firstNameInput.fill(firstName);
+        fillInput(firstNameInput, firstName);
         return this;
     }
 
     public RegistrationPage fillLastNameInput(String lastName) {
-        lastNameInput.fill(lastName);
+        fillInput(lastNameInput, lastName);
         return this;
     }
 
     public RegistrationPage fillEmailInput(String email) {
-        emailInput.fill(email);
+        fillInput(emailInput, email);
         return this;
     }
 
     public RegistrationPage fillPasswordInput(String password) {
-        passwordInput.fill(password);
+        fillInput(passwordInput, password);
         return this;
     }
 
     public RegistrationPage fillConfirmPasswordInput(String confirmPassword) {
-        confirmPasswordInput.fill(confirmPassword);
+        fillInput(confirmPasswordInput, confirmPassword);
         return this;
     }
 }
