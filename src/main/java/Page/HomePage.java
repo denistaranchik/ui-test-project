2011fccd8818e7142d8772e7ce57fae9586c1a32
denistaranchik <taranchikdenis94@gmail.com>
2026-08-18 22:31:01 +0300

    refactor HomePage by updating fill methods for using fillInput method from BasePage / updating getCustomerInfoText and checkLogOutButtonPresence methods with waitUntilElementIsVisible to avoid flaky reads on slow-loading elements / replacing isSortBySelectVisible with searchResultTitle for search assertions

--- src/main/java/Page/HomePage.java
+++ src/main/java/Page/HomePage.java
@@ -2,18 +2,20 @@ package Page;
 
 import com.microsoft.playwright.Locator;
 
-public class HomePage{
+import static Utils.Waiter.waitUntilElementIsVisible;
+
+
+public class HomePage extends BasePage {
 
     private final Locator registrationButton = BasePage.page.locator(".ico-register");
     private final Locator loginButton = BasePage.page.locator(".ico-login");
     private final Locator logoutButton = BasePage.page.locator(".ico-logout");
     private final Locator searchInput = BasePage.page.locator("#small-searchterms");
     private final Locator searchButton = BasePage.page.locator(".button-1.search-box-button");
-    private final Locator customerInfo = BasePage.page.locator("(//a[contains(@class, 'account')])[1]");
-    private final Locator customerInfoAbsence = BasePage.page.locator(".ico-register");
-    private final Locator sortedByFieldSearchValidResult = BasePage.page.locator("#products-orderby");
+    public final Locator customerInfo = BasePage.page.locator("//div[@class='header-links']//a[contains(@class, 'account')]"
+    );
+    public final Locator searchResultTitle = BasePage.page.locator("//h2[@class='product-title']");
 
-    // Click methods
 
     public RegistrationPage clickRegistrationButton() {
         registrationButton.click();
@@ -21,8 +23,8 @@ public class HomePage{
     }
 
     public LoginPage clickLoginButton() {
-           loginButton.click();
-           return new LoginPage();
+        loginButton.click();
+        return new LoginPage();
     }
 
     public HomePage clickLogoutButton() {
@@ -30,33 +32,27 @@ public class HomePage{
         return this;
     }
 
-    public void clickSearchInput() {
-        searchInput.click();
-    }
-
     public HomePage clickSearchButton() {
         searchButton.click();
         return this;
     }
 
-    // Fill methods
-
-    public HomePage fillSearchInput(String input) {
-        searchInput.fill(input);
+    public HomePage fillSearchInput(String query) {
+        fillInput(searchInput, query);
         return this;
     }
 
-    // Assertions
-
     public String getCustomerInfoText() {
+        waitUntilElementIsVisible(customerInfo);
         return customerInfo.innerText();
     }
 
-    public String checkCustomerInfoTextAbsence() {
-        return customerInfoAbsence.innerText();
+    public String checkRegistrationButtonPresence() {
+        return registrationButton.innerText();
     }
 
     public String checkLogOutButtonPresence() {
+        waitUntilElementIsVisible(customerInfo);
         return logoutButton.innerText();
     }
 
@@ -64,10 +60,10 @@ public class HomePage{
         return loginButton.innerText();
     }
 
-    public boolean isSortBySelectVisible() {
-        return sortedByFieldSearchValidResult.isVisible();
+    public String searchResultTitle() {
+        return searchResultTitle.innerText();
     }
 
-    }
+}
 
 
